75439069b2c1070c9ff84318f24680b024106d20
Reinbert <reinhard.sprung@gmail.com>
2021-05-01 00:01:12 +0200

    Transitioning to micro-ros for ros2 foxy. (It seems I can't use platformio for that :-/)

---
+++ app.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 msg;
+
+void subscription_callback(const void * msgin)
+{
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+	printf("Received: %d\n", msg->data);
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+    RCLC_UNUSED(last_call_time);
+    if (timer != NULL) {
+        printf("Timer: %" PRId64 "\n", last_call_time);
+    }
+}
+
+void appMain(void * arg)
+{
+  	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "ros_esp32cam_diffdrive", "", &support));
+
+	// create subscriber
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"/microROS/int32_subscriber"));
+
+    // create timer,
+    rcl_timer_t timer;
+    const unsigned int timer_timeout = 1000;
+    RCCHECK(rclc_timer_init_default(
+            &timer,
+            &support,
+            RCL_MS_TO_NS(timer_timeout),
+            timer_callback));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+	while(1){
+			rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+			usleep(100000);
+	}
+
+	// free resources
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_node_fini(&node));
+	
+	vTaskDelete(NULL);
+}
